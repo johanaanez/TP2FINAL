9beb98309880b7e6066f01de9287e8f1c7dd785b
johanaanez <johana.anez@gmail.com>
2014-01-30 14:11:10 -0300

    agregue los estaticos

---
+++ JuegoGpsChallenge/src/aplicacion/main/Principal.java
@@ -0,0 +1,19 @@
+package aplicacion.main;
+
+import aplicacion.controlador.Controlador;
+import aplicacion.modelo.juego.GPSChallenge;
+import aplicacion.modelo.juego.dificultad.*;
+import aplicacion.modelo.vehiculos.*;
+import aplicacion.vista.VistaBienvenida;
+import aplicacion.vista.VistaGPS;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		// creamos la vista
+		VistaBienvenida vista = new VistaBienvenida("GPS CHALLENGE",400,400);
+		vista.saludar();
+	}
+
+}
